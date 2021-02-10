0749320d02377d46d1b20684f87ee663e24d25e3
Denis <osadchydenis@gmail.com>
2021-02-10 19:05:28 +0200

    test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {Provider} from  'react-redux';
 import {store, persistor} from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
+//asd
+
 const app = (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor} >
